68c790ddddbdc03e023097e1f7640e8346f50b04
Bielikov Ivan <ivanbelikov890@gmail.com>
2022-05-19 12:23:07 +0300

    Update settings.py

--- statClashRoyale/statClashRoyale/settings.py
+++ statClashRoyale/statClashRoyale/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)8=!pu&et#d-8i%$=v0^j+(vq1b4(ib@xqt*zs53-8ikt#1$i3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
